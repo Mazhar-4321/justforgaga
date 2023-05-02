05758d453cb57f91fcf08b38c9689196b7c5e2d6
Mazhar-4321 <syedmazharali742@gmail.com>
2023-05-02 10:21:42 +0530

    [Mazhar]MODIFIED: AdminController

--- project/src/controllers/admin.controller.js
+++ project/src/controllers/admin.controller.js
@@ -15,9 +15,61 @@ export const addCourse = async (req, res, next) => {
   }
 };
 
+export const updateCourse = async (req, res, next) => {
+  console.log("reached")
+  try {
+   var data= await AdminService.updateCourse(req);
+    res.status(HttpStatus.OK).json({
+      code: HttpStatus.OK,
+      data: data,
+      message: 'Courses Added successfully'
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+export const checkFiles = async (req, res, next) => {
+  try {
+    await AdminService.checkFiles(req.body);
+    res.status(HttpStatus.OK).json({
+      code: HttpStatus.OK,
+      data: data,
+      message: 'Courses Added successfully'
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+export const getCourseById = async (req, res, next) => {
+  try {
+   var data= await AdminService.getCourseById(req.params.id);
+    res.status(HttpStatus.OK).json({
+      code: HttpStatus.OK,
+      data: data,
+      message: 'Courses Added successfully'
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 export const deleteCourse = async (req, res, next) => {
     try {
       await AdminService.deleteCourse(req.params.id);
+      res.status(HttpStatus.OK).json({
+        code: HttpStatus.OK,
+        data: [],
+        message: 'Courses Deleted successfully'
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  export const deleteNoteById=async (req, res, next) => {
+    try {
+      console.log("reached")
+      await AdminService.deleteNoteById(req.body);
       res.status(HttpStatus.OK).json({
         code: HttpStatus.OK,
         data: [],
@@ -59,6 +111,20 @@ export const deleteCourse = async (req, res, next) => {
   export const getCertificateRequests=async(req,res,next)=>{
     try {
       var data= await AdminService.getCertificateRequests(req.params.id);
+      console.log("data sent",data)
+       res.status(HttpStatus.OK).json({
+         code: HttpStatus.OK,
+         data: data,
+         message: 'Courses Fetched successfully'
+       });
+     } catch (error) {
+       next(error);
+     }
+  }
+
+  export const getDashBoardDetails=async(req,res,next)=>{
+    try {
+      var data= await AdminService.getDashBoardDetails(req.params.id);
       console.log("data sent",data)
        res.status(HttpStatus.OK).json({
          code: HttpStatus.OK,
--- project/src/routes/admin.route.js
+++ project/src/routes/admin.route.js
@@ -6,16 +6,24 @@ const router = express.Router();
 
 router.post('/addCourse', AdminController.addCourse);
 
+
+
 router.get('/courses/:id', AdminController.getAllCourses);
 
 
 router.delete('/courses/:id', AdminController.deleteCourse);
-
+router.post('/deleteNoteById', AdminController.deleteNoteById);
 router.post('/addQuiz/:id',AdminController.addQuiz);
 
 router.get('/certificateRequests/:id',AdminController.getCertificateRequests)
 
+router.get('/courseById/:id',AdminController.getCourseById);
 
+router.post('/checkFiles',AdminController.updateCourse);
+
+
+router.put('/updateCourse', AdminController.updateCourse);
+ router.get('/dashboard/:id',AdminController.getDashBoardDetails)
 
 
 
